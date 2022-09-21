df3d667e8b0a7051247ad05537edef8e58bc4315
Steven Attipoe <stevenattipoe7@gmail.com>
2022-09-21 13:33:53 +0000

    initial commit

---
+++ src/main/java/sea/nat/ashesi/healthhubservice/HealthHubServiceApplication.java
@@ -0,0 +1,13 @@
+package sea.nat.ashesi.healthhubservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthHubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sea/nat/ashesi/healthhubservice/HealthHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sea.nat.ashesi.healthhubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
